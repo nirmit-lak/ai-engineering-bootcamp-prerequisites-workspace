fed6bb871604a6c7d235167e0351a62a4965486a
Nirmit Maheshwari <nirmish@Nirmits-MacBook-Pro.local>
2026-07-26 16:33:22 +0530

    Add streamlit ui and docker

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ai-engineering-bootcamp-prerequisites-workspace!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+from openai import OpenAI
+from groq import Groq
+from google import genai
+from core.config import config
+
+def run_llm(provider, model_name, messages, max_tokens=500) :
+
+    if provider == "OpenAI" :
+        client = OpenAI(api_key=config.OPENAI_API_KEY)
+    elif provider == "Groq" :
+        client = Groq(api_key=config.GROQ_API_KEY)
+    else :
+        client = genai.Client(api_key=config.GOOGLE_API_KEY)
+
+    if provider == "Google" :
+        return client.models.generate_content(
+            model=model_name,
+            contents=[message["content"] for message in messages],
+        ).text 
+    elif provider == "Groq" :
+        return client.chat.completions.create(
+            model=model_name,
+            messages=messages,
+            max_completion_tokens=max_tokens
+        ).choices[0].message.content
+    else :
+        return client.chat.completions.create(
+            model=model_name,
+            messages=messages,
+            max_completion_tokens=max_tokens,
+            reasoning_effort="minimal"
+        ).choices[0].message.content
+
+
+## Lets create a sidebar with a dropdown for the model list and providers
+with st.sidebar :
+    st.title("settings");
+
+    # dropdown for model
+    provider = st.selectbox("Select a provider", ["OpenAI", "Groq", "Google"])
+    if provider == "OpenAI" :
+        model_name = st.selectbox("Select a model", ["gpt-5.6-sol","gpt-5.6-terra","gpt-5.6-luna"])
+    elif provider == "Groq" :
+        model_name = st.selectbox("Select a model", ["llama-3.3-70b-versatile", "groq-1.1"])
+    else :
+        model_name = st.selectbox("Select a model", ["gemini-3.6-flash", "gemini-3.5-turbo"])
+
+    # Save provider and model to session state
+    st.session_state["provider"] = provider
+    st.session_state["model_name"] = model_name
+
+if "messages"  not in st.session_state :
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "Hello! I am your AI assistant. How can I help you today?"}
+    ]
+
+# Display the chat messages from the session state
+for message in st.session_state["messages"] :
+    with st.chat_message(message["role"]) :
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Hello! I am your AI assistant. How can I help you today?") :
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user") :
+        st.markdown(prompt)
+
+    with st.chat_message("assistant") :
+        output = run_llm(st.session_state["provider"], st.session_state["model_name"], st.session_state["messages"])
+        response_data = output
+        answer = response_data
+        st.write(answer)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
