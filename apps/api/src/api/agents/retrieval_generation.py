0b419eebb193e7e4d5670f4589f2a3d3a7b904c2
Nirmit Maheshwari <nirmish@Nirmits-MacBook-Pro.local>
2026-08-02 17:35:41 +0530

    9. endpoint for RAG

---
+++ apps/api/src/api/agents/retrieval_generation.py
@@ -0,0 +1,90 @@
+import openai
+from qdrant_client import QdrantClient
+
+def get_embedding(text, model="text-embedding-3-small"):
+    response = openai.embeddings.create(
+        input=text,
+        model=model
+    )
+    return response.data[0].embedding
+
+def retrieve_data(query,qdrant_client, k=5) :
+
+    query_embedding = get_embedding(query)
+
+    results = qdrant_client.query_points(
+        collection_name="Amazon-items-collection-00",
+        query=query_embedding,
+        limit=k
+    )
+
+    retrieved_context_ids = []
+    retrieved_context = []
+    similarity_scores = []
+    retrieved_context_ratings = []
+
+    for result in results.points : 
+        retrieved_context_ids.append(result.payload["parent_asin"])
+        retrieved_context.append(result.payload["description"])
+        retrieved_context_ratings.append(result.payload["average_rating"])
+        similarity_scores.append(result.score)
+    
+    return {
+        "retrieved_context_ids" : retrieved_context_ids,
+        "retrieved_context" : retrieved_context,
+        "retrieved_context_ratings" : retrieved_context_ratings,
+        "similarity_scores" : similarity_scores
+    }
+
+
+def process_context(context):
+
+    formatted_context = ""
+
+    for id, chunk , rating in zip(context["retrieved_context_ids"], context["retrieved_context"], context['retrieved_context_ratings']):
+        formatted_context += f"- ID: {id}, rating: {rating}, description: {chunk}\n"
+
+    return formatted_context
+
+def build_prompt(preprocessed_context, question):
+
+    prompt = f"""
+You are a shopping assistant that can answer questions about the products in stock.
+
+You will be given a question and a list of context.
+
+Instructions :
+- You need to answer the question based on the provided context only.
+- Never use word context and refer to it as the available products.
+
+Context:
+{preprocessed_context}
+
+Question:
+{question}
+"""
+
+    return prompt
+
+def generate_answer(prompt) :
+
+    response = openai.chat.completions.create(
+        model="gpt-5.6-luna",
+        messages=[{"role":"system", "content": prompt}],
+        reasoning_effort="low"
+    )
+
+    return response.choices[0].message.content
+
+
+def rag_pipeline(question, top_k=5):
+
+    qdrant_client = QdrantClient(url="http://qdrant:6333")
+
+    retrieved_context = retrieve_data(question, qdrant_client, top_k)
+    preprocessed_context = process_context(retrieved_context)
+    prompt = build_prompt(preprocessed_context, question)
+    answer = generate_answer(prompt)
+
+    return answer
+
--- apps/chatbot_ui/src/chatbot_ui/app.py
+++ apps/chatbot_ui/src/chatbot_ui/app.py
@@ -35,23 +35,6 @@ def api_call(method,url,**kwargs) :
         _show_error_popup(f"An error occurred: {str(e)}")
         return False, {"message": str(e)}
 
-## Lets create a sidebar with a dropdown for the model list and providers
-with st.sidebar :
-    st.title("settings");
-
-    # dropdown for model
-    provider = st.selectbox("Select a provider", ["OpenAI", "Groq", "Google"])
-    if provider == "OpenAI" :
-        model_name = st.selectbox("Select a model", ["gpt-5.6-sol","gpt-5.6-terra","gpt-5.6-luna"])
-    elif provider == "Groq" :
-        model_name = st.selectbox("Select a model", ["llama-3.3-70b-versatile", "groq-1.1"])
-    else :
-        model_name = st.selectbox("Select a model", ["gemini-3.6-flash", "gemini-3.5-turbo"])
-
-    # Save provider and model to session state
-    st.session_state["provider"] = provider
-    st.session_state["model_name"] = model_name
-
 if "messages"  not in st.session_state :
     st.session_state["messages"] = [
         {"role": "assistant", "content": "Hello! I am your AI assistant. How can I help you today?"}
@@ -68,8 +51,8 @@ if prompt := st.chat_input("Hello! I am your AI assistant. How can I help you to
         st.markdown(prompt)
 
     with st.chat_message("assistant") :
-        output = api_call("post", f"{config.API_URL}/chat", json={"provider": st.session_state["provider"], "model_name": st.session_state["model_name"], "messages": st.session_state["messages"]})
+        output = api_call("post", f"{config.API_URL}/rag", json={"query": prompt})
         response_data = output[1]
-        answer = response_data["message"]
+        answer = response_data["answer"]
         st.write(answer)
     st.session_state.messages.append({"role": "assistant", "content": answer})
